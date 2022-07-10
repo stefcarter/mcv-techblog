fa95de8a1690ca0946b398080dace152e7ee1202
Steffan Carter <xisled09@gmail.com>
2022-07-10 19:39:33 -0400

    server console log for port added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Supedr secret secret',
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
@@ -33,5 +33,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'))
+    app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}/`));
 })
