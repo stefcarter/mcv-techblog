15b90311558aff1e0dca53831f5b672849268049
Steffan Carter <xisled09@gmail.com>
2022-07-25 23:56:48 -0400

    fixed get error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
-const sequelize = require('../config/connection');
+const { application } = require('express');
+// const sequelize = require('../config/connection');
 const router = require('express').Router();
 const { User } = require('../models');
 
